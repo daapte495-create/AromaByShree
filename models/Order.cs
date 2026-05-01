23df56bdf5bea8185c43702704c557ebdd7e2e19
daapte495-create <daapte495@rku.ac.in>
2026-05-01 13:48:34 +0530

    Add files via upload

---
+++ models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShreePerfume.Models
+{
+    public class Order 
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; } // Login kiye huye user ki ID
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = "Pending"; // Pending, Shipped, Delivered
+
+
+        public virtual User? User { get; set; }
+
+        // Shipping details
+        public string? ShippingAddress { get; set; }
+        public string? City { get; set; }
+        public string? Phone { get; set; }
+    }
+}
