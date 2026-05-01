e413ea89195e1dc9b6dcda3fecd78b2cb33b0bc3
daapte495-create <daapte495@rku.ac.in>
2026-05-01 13:49:15 +0530

    Add files via upload

---
+++ models/Perfume.cs
@@ -0,0 +1,12 @@
+﻿namespace ShreePerfume.Models
+{
+    public class Perfume
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Gender { get; set; } // "Men" or "Women"
+    }
+}
