8e12f2b041d63c310e2205be19e6857080137ce7
daapte495-create <daapte495@rku.ac.in>
2026-05-01 13:49:37 +0530

    Add files via upload

---
+++ models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShreePerfume.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        [Range(1, 5)]
+        public int Rating { get; set; } // 1 to 5 Stars
+
+        [Required]
+        public string Comment { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
