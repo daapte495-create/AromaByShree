1c6bc4d1ba537a0cf6e2d58240786b79fb6a24b8
daapte495-create <daapte495@rku.ac.in>
2026-05-01 13:46:26 +0530

    Add files via upload

---
+++ models/AccountViewModels.cs
@@ -0,0 +1,44 @@
+﻿//AccountViewModels.cs file
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ShreePerfume.Models
+{
+    public class LoginViewModel
+    {
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        public string? FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required, Phone]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid 10-digit phone number")]
+        public string? PhoneNo { get; set; }
+
+        [Required]
+        public string? Gender { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+
+        public IFormFile? ProfilePhoto { get; set; } // Optional
+    }
+}
---
+++ models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿//cartItem.cs file
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShreePerfume.Models
+{
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
