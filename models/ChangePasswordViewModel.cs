641114b583461d27d972e2d3222b9af5e89dafd1
daapte495-create <daapte495@rku.ac.in>
2026-05-01 13:47:54 +0530

    Add files via upload

---
+++ models/ChangePasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShreePerfume.Models
+{
+    public class ChangePasswordViewModel
+    {
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
---
+++ models/ContactForm.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShreePerfume.Models
+{
+    public class ContactForm
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Message cannot be empty")]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime SubmittedAt { get; set; } = DateTime.Now;
+
+        public bool IsRead { get; set; } = false;
+        public string? AdminReply { get; set; }
+        //public DateTime? RepliedAt { get; set; }
+
+
+    }
+}
