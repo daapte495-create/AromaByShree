08197f1d67f7d63c90d71aea698368f7d3dfa093
shruti785119 <shrutiparekh785@gmail.com>
2026-05-04 09:27:01 +0530

    Add files via upload

---
+++ viewcomponents/CartCountViewComponent.cs
@@ -0,0 +1,26 @@
+﻿//ViewComponents/ CartCountViewComponent.cs file
+
+using Microsoft.AspNetCore.Mvc;
+using ShreePerfume.Models;
+using System.Text.Json;
+
+namespace ShreePerfume.ViewComponents
+{
+    public class CartCountViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var sessionData = HttpContext.Session.GetString("Cart");
+            int count = 0;
+
+            if (!string.IsNullOrEmpty(sessionData))
+            {
+                var cart = JsonSerializer.Deserialize<List<CartItem>>(sessionData);
+                // Sum up all quantities in the cart
+                count = cart?.Sum(x => x.Quantity) ?? 0;
+            }
+
+            return View(count);
+        }
+    }
+}
